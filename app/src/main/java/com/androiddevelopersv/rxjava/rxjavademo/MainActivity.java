fa1e2431b5185a128ef797b8a3f00d5f575d698f
ragar90 <rene.garciah90@gmail.com>
2019-01-18 09:22:28 -0600

    Step 0: Setup

---
+++ app/src/main/java/com/androiddevelopersv/rxjava/rxjavademo/MainActivity.java
@@ -0,0 +1,13 @@
+  package com.androiddevelopersv.rxjava.rxjavademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
